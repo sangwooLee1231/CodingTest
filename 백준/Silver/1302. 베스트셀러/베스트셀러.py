1a5822964a1b78d61d54ff7026612f2bf691a013
이상우 <tkdguq0654@naver.com>
2025-05-06 17:02:15 +0900

    [Silver IV] Title: 베스트셀러, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.py
@@ -0,0 +1,15 @@
+d = dict()
+for _ in range(int(input())):
+    book = input()
+    if book in d:
+        d[book] += 1
+    else:
+        d[book] = 1
+
+m = max(d.values())
+candi = []
+for k,v in d.items():
+    if v ==m:
+        candi.append(k)
+
+print(sorted(candi)[0])
