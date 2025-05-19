3a65baef88e3d0d8882e8ed83a7bc4ab81f21fbe
이상우 <tkdguq0654@naver.com>
2025-05-19 18:20:19 +0900

    [Silver III] Title: 수리공 항승, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+coord = [False] * 1001
+N, L = map(int,input().split())
+for i in map(int,input().split()):
+    coord[i] = True
+
+
+ans = 0
+x = 0
+while x < 1001:
+    if coord[x]:
+        ans += 1
+        x += L
+    else:
+        x += 1
+print(ans)
